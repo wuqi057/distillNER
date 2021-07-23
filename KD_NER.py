10267dcd5e7553bc1cdb375c1f46418d4b3fecd9
Qi Wu <Wu@Mac0041.local>
2021-07-23 16:10:29 +0200

    add

--- KD_NER.py
+++ KD_NER.py
@@ -27,23 +27,23 @@ sentences_conll = []
 for sent in corpus.train:
     sentences_conll.append(sent.to_original_text())
 #%% select Nr of sentences in each dataset and mix them 
-Nr = 1000
-sentences = sentences_conll[0:Nr] + sentences_news[0:Nr]
-#%%
-# predict data with ner_large 
-from flair.data import Sentence
-from tqdm import tqdm
-data = []
-for sentence in tqdm(sentences):
-    if sentence != '':
-        sentence = Sentence(sentence)
-        teacher.predict(sentence,all_tag_prob=True)
-        data.append(sentence)
+# Nr = 1000
+# sentences = sentences_conll[0:Nr] + sentences_news[0:Nr]
+# #%%
+# # predict data with ner_large 
+# from flair.data import Sentence
+# from tqdm import tqdm
+# data = []
+# for sentence in tqdm(sentences):
+#     if sentence != '':
+#         sentence = Sentence(sentence)
+#         teacher.predict(sentence,all_tag_prob=True)
+#         data.append(sentence)
 
-#%% save the data(together with the distribution inside the object) to trainset
-import pickle
-with open(path+'data/data.pickle', 'wb') as handle:
-    pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# #%% save the data(together with the distribution inside the object) to trainset
+# import pickle
+# with open(path+'data/data.pickle', 'wb') as handle:
+#     pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 # #%%
 # before_predict = corpus.train[6].get_spans('ner')#[0][0].get_tags_proba_dist('ner')
